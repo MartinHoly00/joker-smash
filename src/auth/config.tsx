55d70088afbf74b86411ae7645c1e41f8c168902
Martin Holý <115696258+MartinHoly00@users.noreply.github.com>
2025-11-06 22:49:57 +0100

    shh

--- src/auth/config.tsx
+++ src/auth/config.tsx
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCo1uhmkgAWRwFhkKPR0zrSg1CD6DDzUug",
-  authDomain: "joker-smash-9de16.firebaseapp.com",
-  projectId: "joker-smash-9de16",
-  storageBucket: "joker-smash-9de16.firebasestorage.app",
-  messagingSenderId: "975052293033",
-  appId: "1:975052293033:web:918eef7b972c04be70bab8",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
